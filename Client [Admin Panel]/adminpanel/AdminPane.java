5bb4176e334b8636a1cbee9429f968acdd4da4c5
afsheen <afsheen_rane@hotmail.com>
2015-07-11 03:30:54 -0400

    Mostly finished building AdminPane. Added some I/O functionality to MainClientFrame.

--- Client [Admin Panel]/adminpanel/AdminPane.java
+++ Client [Admin Panel]/adminpanel/AdminPane.java
@@ -1,10 +1,99 @@
 package adminpanel;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class AdminPane extends JPanel {
 
-    public AdminPane() {
-        // TODO Auto-generated constructor stub
+    private final MainClientFrame parentFrame;
+
+    private JLabel statusLab;
+
+    private JPanel topPane;
+    private JPanel botPane;
+
+    private JButton showBut;
+    private JButton hideBut;
+    private JButton killBut;
+
+    public AdminPane(MainClientFrame mainClientFrame) {
+        super();
+        parentFrame = mainClientFrame;
+
+        setLayout(new BorderLayout());
+
+        initComponents();
+        addHandlers();
+        addComponents();
+    }
+
+    private void initComponents() {
+        topPane = new JPanel();
+        botPane = new JPanel();
+
+        showBut = new JButton("SHOW RED BUTTON");
+        hideBut = new JButton("HIDE RED BUTTON");
+        killBut = new JButton("SHUTDOWN BUTTON");
+    }
+
+    private void addHandlers() {
+        ButtonHandler buttonHandler = new ButtonHandler();
+
+        showBut.addActionListener(buttonHandler);
+        hideBut.addActionListener(buttonHandler);
+        killBut.addActionListener(buttonHandler);
+
+    }
+
+    private void addComponents() {
+
+        topPane.add(showBut);
+        topPane.add(hideBut);
+        topPane.add(killBut);
+
+        botPane.add(statusLab);
+
+        add(topPane, BorderLayout.CENTER);
+        add(botPane, BorderLayout.SOUTH);
+
+    }
+
+    private int checkKillButton() {
+        return JOptionPane
+                .showConfirmDialog(
+                        null,
+                        "Are you sure you want to close the Big Red Button?\n"
+                                + "This will disconnect you and all other computers which are connected to it.",
+                        "Kill Button Confirmation", JOptionPane.WARNING_MESSAGE);
+    }
+
+    public void updateStatusLabel(String status) {
+        // TODO
+    }
+
+    private class ButtonHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == showBut) {
+                parentFrame.tellServer("showbutton");
+            }
+            else if (e.getSource() == hideBut) {
+                parentFrame.tellServer("hidebutton");
+            }
+            else if (e.getSource() == killBut) {
+                if (checkKillButton() == JOptionPane.OK_OPTION) {
+                    parentFrame.tellServer("kill");
+                }
+            }
+        }
+
     }
 }
--- Client [Admin Panel]/adminpanel/MainClientFrame.java
+++ Client [Admin Panel]/adminpanel/MainClientFrame.java
@@ -1,5 +1,7 @@
 package adminpanel;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +10,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MainClientFrame {
     private final JFrame mainFrame;
@@ -17,10 +20,15 @@ public class MainClientFrame {
     private PrintWriter out;
 
     private final SearchPane searchPane;
+    private AdminPane adminPane;
 
     private final int PORT = 2222;
+    private String buttonStatus;
 
     public MainClientFrame() {
+
+        buttonStatus = " ";
+
         mainFrame = new JFrame();
         searchPane = new SearchPane(this);
 
@@ -41,16 +49,97 @@ public class MainClientFrame {
             in = new BufferedReader(new InputStreamReader(
                     socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
+
             // TODO open admin panel
+            addAdminPane();
+            listenForInput();
+
         }
         catch (UnknownHostException e) {
+            closeAll();
             searchPane.setStatusLabText("Cannot find host: " + userInput);
 
         }
         catch (IOException e) {
+            closeAll();
             searchPane
                     .setStatusLabText("Cannot connect to host, check if server active.");
         }
+    }
+
+    private void addAdminPane() {
+        adminPane = new AdminPane(this);
+
+        mainFrame.remove(searchPane);
+        mainFrame.add(adminPane);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (checkQuit() == JOptionPane.OK_OPTION) {
+                    closeAll();
+                    mainFrame.dispose();
+                    System.exit(0);
+                }
+            }
+
+        });
+
+        mainFrame.pack();
+        mainFrame.revalidate();
+        mainFrame.setVisible(true);
+    }
+
+    private void listenForInput() {
+        buttonStatus = " ";
+
+        try {
+            while ((buttonStatus = in.readLine()) != null) {
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void closeAll() {
+        try {
+            socket.close();
+            in.close();
+            out.close();
+        }
+        catch (IOException e) {
+        }
+
+    }
+
+    /**
+     * Sends the big red button server a command.
+     * 
+     * @param command the command to send.
+     */
+    public void tellServer(String command) {
+        out.println(command);
+    }
+
+    /**
+     * @return the buttonStatus
+     */
+    public String getButtonStatus() {
+        return buttonStatus;
+    }
+
+    public int checkQuit() {
+        if (buttonStatus.equalsIgnoreCase("buttondead"))
+            return JOptionPane.OK_OPTION;
+
+        return JOptionPane
+                .showConfirmDialog(
+                        null,
+                        "Closing this will cause you to disconnect from the Big Red Button.\nAre you sure you want to exit?",
+                        "Shutdown confirmation", JOptionPane.WARNING_MESSAGE);
     }
 
     public static void main(String[] args) {
